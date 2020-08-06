33c83c657b8052f5024ff2b10881031ce6510b34
John Walthour <john.walthour@gmail.com>
2020-08-06 19:15:43 -0400

    Made another worker thread to offload the filtering process from the listener threads.  Need to put the filters back in though.

--- PassiveAggressorDotNet/PassiveAggressor/NetworkMonitor.cs
+++ PassiveAggressorDotNet/PassiveAggressor/NetworkMonitor.cs
@@ -16,7 +16,7 @@ namespace PassiveAggressor
         /// <summary>
         /// Fire update event no more often than this
         /// </summary>
-        public double MinUpdateIntervalSeconds { get; set; } = 0.1;
+        public double UpdateIntervalSeconds { get; set; } = 0.1;
 
         /// <summary>
         /// Time we most recently fired HostListChanged
@@ -28,13 +28,30 @@ namespace PassiveAggressor
         /// </summary>
         public class Host
         {
+            public Host(PcapDotNet.Packets.Ethernet.MacAddress mac, PcapDotNet.Packets.IpV4.IpV4Address host, DeviceAddress intf)
+            {
+                LastSeen = DateTime.Now;
+                HostMacAddress = mac;
+                HostIpV4Address = host;
+                IntfIpV4Address = intf;
+            }
             public DateTime LastSeen;
-            public PcapDotNet.Packets.IpV4.IpV4Address? HostIpV4Address = null;
-            public PcapDotNet.Packets.IpV4.IpV4Address? IntfIpV4Address = null;
+            public PcapDotNet.Packets.Ethernet.MacAddress HostMacAddress;
+            public PcapDotNet.Packets.IpV4.IpV4Address HostIpV4Address;
+            public DeviceAddress IntfIpV4Address;
             // TODO: IPv6 support
             //public PcapDotNet.Packets.IpV6.IpV6Address? IpV6Address = null;
         }
 
+
+        /// <summary>
+        /// Host observations that still need to be checked and incorporated into the Hosts dictionary
+        /// </summary>
+        private Queue<Host> hostsToIncorporate = new Queue<Host>();
+
+        /// <summary>
+        /// Hosts that are ready to deliver (that is, confirmed to be local addresses)
+        /// </summary>
         private Dictionary<PcapDotNet.Packets.Ethernet.MacAddress, Host> Hosts = new Dictionary<PcapDotNet.Packets.Ethernet.MacAddress, Host>();
 
         /// <summary>
@@ -53,7 +70,7 @@ namespace PassiveAggressor
         public class Interface
         {
             public PacketCommunicator Communicator;
-            public PcapDotNet.Packets.IpV4.IpV4Address? IpV4Address = null;
+            public DeviceAddress IpV4Address = null;
             public LivePacketDevice Device;
             public BackgroundWorker MonitorWorker;
         }
@@ -73,6 +90,11 @@ namespace PassiveAggressor
         /// Check this often for worker cancellation
         /// </summary>
         private const int PACKET_RX_TIMEOUT_MS = 100;
+
+        /// <summary>
+        /// Intended to perform any CPU-bound work to free up the other threads to listen for packets
+        /// </summary>
+        private BackgroundWorker packetProcessorWorker;
 
         /// <summary>
         /// Find interfaces and open them all for listening
@@ -97,15 +119,12 @@ namespace PassiveAggressor
                 {
                     if (addr.Address.Family == SocketAddressFamily.Internet)
                     {
-                        // Hacky conversion
-                        string addrStr = addr.Address.ToString();
-                        intf.IpV4Address = new PcapDotNet.Packets.IpV4.IpV4Address(addrStr.Substring("Internet ".Length));
+                        intf.IpV4Address = addr;
                     }
-                    // TODO: IPv6 support
                 }
 
                 intf.Communicator = device.Open(PACKET_RX_LEN_B, PacketDeviceOpenAttributes.Promiscuous, PACKET_RX_TIMEOUT_MS);
-                // Filter to only IPv4 packets so we can assume they have an IP header later
+                // Filter to only IPv4 packets so we can assume they have an IPv4 header later
                 using (BerkeleyPacketFilter filter = intf.Communicator.CreateFilter("ip"))
                 {
                     intf.Communicator.SetFilter(filter);
@@ -127,7 +146,69 @@ namespace PassiveAggressor
                     Interfaces.Add(device.Name, intf);
                 }
             }
+
+            packetProcessorWorker = new BackgroundWorker();
+            packetProcessorWorker.DoWork += incorporatePackets;
+            packetProcessorWorker.WorkerSupportsCancellation = true;
+            packetProcessorWorker.WorkerReportsProgress = false;
+            packetProcessorWorker.RunWorkerAsync();
         }
+
+        /// <summary>
+        /// Check hosts added to hostsToIncorporate and incorporate them into the Hosts dictionary
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void incorporatePackets(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            try
+            {
+                while (!worker.CancellationPending)
+                {
+                    while (hostsToIncorporate.Count > 0) // assumes Queue.Count is atomic and thus automatically thread-safe
+                    {
+                        Host host = null;
+
+                        // This lock needs to be very short because the listener threads aren't listening while they wait for the lock
+                        lock (hostsToIncorporate)
+                        {
+                            if (hostsToIncorporate.Count > 0)
+                            {
+                                host = hostsToIncorporate.Dequeue();
+                            }
+                        }
+
+                        if (host != null)
+                        {
+                            // Is this from the same subnet as the interface on which it was captured?
+
+                            // Is this outbound from the interface on which it was captured?
+                            Hosts[host.HostMacAddress] = host;
+
+                            if (DateTime.Now > lastUpdateTime.AddSeconds(UpdateIntervalSeconds))
+                            {
+                                HostListChanged?.Invoke(Hosts);
+                                lastUpdateTime = DateTime.Now;
+                            }
+                            else
+                            {
+                                //Console.WriteLine("Too soon for update");
+                            }
+                        }
+                    }
+                    // An inelegant way to avoid spinlock - sleep for about 1/1000th of the update interval
+                    System.Threading.Thread.Sleep((int)(UpdateIntervalSeconds) + 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught exception in listener thread: " + ex);
+            }
+
+        }
+
 
         /// <summary>
         /// Loop through packets until cancelled
@@ -138,57 +219,38 @@ namespace PassiveAggressor
         {
             BackgroundWorker worker = sender as BackgroundWorker;
             Interface intf = e.Argument as Interface;
-
-            while (!worker.CancellationPending)
+            try
             {
-
-                Packet packet;
-                // TODO: Want to return to listening super fast rather than spending time on processing after
-                PacketCommunicatorReceiveResult result = intf.Communicator.ReceivePacket(out packet);
-                //communicator.ReceiveSomePackets()
-                switch (result)
+                while (!worker.CancellationPending)
                 {
-                    case PacketCommunicatorReceiveResult.Timeout:
-                        // Timeout elapsed
-                        break;
-                    case PacketCommunicatorReceiveResult.Ok:
-                        incorporatePacket(packet, intf);
-                        //Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" +
-                        //                  packet.Length);
-                        break;
-                    default:
-                        throw new InvalidOperationException("The result " + result + " should never be reached here");
+
+                    Packet packet;
+                    // We're only listening during the below function call.  Thus, all of this thread outside of this function call should be as fast as it can be.
+                    PacketCommunicatorReceiveResult result = intf.Communicator.ReceivePacket(out packet);
+                    //communicator.ReceiveSomePackets()
+                    switch (result)
+                    {
+                        case PacketCommunicatorReceiveResult.Timeout:
+                            // Timeout elapsed
+                            break;
+                        case PacketCommunicatorReceiveResult.Ok:
+                            // Chuck it in the queue to evaluate on another thread
+                            Host host = new Host(packet.Ethernet.Source, packet.Ethernet.IpV4.Source, intf.IpV4Address);
+                            lock (hostsToIncorporate)
+                            {
+                                hostsToIncorporate.Enqueue(host);
+                            }
+                            break;
+                        default:
+                            throw new InvalidOperationException("The result " + result + " should never be reached here");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught exception in listener thread: " + ex);
             }
         }
 
-        private void incorporatePacket(Packet packet, Interface rxIntf)
-        {
-            if (packet.Ethernet.IpV4.Source.Equals(rxIntf.IpV4Address))
-            {
-                //Console.WriteLine("Ignoring packet; it's from us");
-            }
-            else
-            {
-                //Console.WriteLine("Saw packet from MAC " + packet.Ethernet.Source);
-                Host host = new Host();
-                host.HostIpV4Address = packet.Ethernet.IpV4.Source;
-                host.IntfIpV4Address = rxIntf.IpV4Address;
-                host.LastSeen = DateTime.Now;
-
-                // TODO: mutex for thread safety
-                Hosts[packet.Ethernet.Source] = host;
-
-                if (DateTime.Now > lastUpdateTime.AddSeconds(MinUpdateIntervalSeconds))
-                {
-                    HostListChanged?.Invoke(Hosts);
-                    lastUpdateTime = DateTime.Now;
-                }
-                else
-                {
-                    //Console.WriteLine("Too soon for update");
-                }
-            }
-        }
     }
 }
