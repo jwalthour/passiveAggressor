eed78f9c13235183fa18ff468d05be7e175cfa40
John Walthour <john.walthour@gmail.com>
2020-08-07 19:42:49 -0400

    Refactor with more encapsulation to support starting and stopping individual interfaces more easily

--- PassiveAggressor/Network/NetworkMonitor.cs
+++ PassiveAggressor/Network/NetworkMonitor.cs
@@ -69,10 +69,153 @@ namespace PassiveAggressor
         /// </summary>
         public class Interface
         {
-            public PacketCommunicator Communicator;
-            public DeviceAddress IpV4Address = null;
-            public LivePacketDevice Device;
-            public BackgroundWorker MonitorWorker;
+            private PacketCommunicator Communicator = null;
+            public DeviceAddress IpV4Address { get; private set; } = null;
+            private LivePacketDevice Device;
+            private BackgroundWorker MonitorWorker;
+
+            /// <summary>
+            /// Reference to the shared queue being written to by all interface listeners and
+            /// read by the consumer
+            /// </summary>
+            private Queue<Host> outputQueue;
+
+
+            /// <summary>
+            /// Receive the first this many bytes of each packet
+            /// </summary>
+            private const int PACKET_RX_LEN_B = 1024;
+
+            /// <summary>
+            /// Check this often for worker cancellation
+            /// </summary>
+            private const int PACKET_RX_TIMEOUT_MS = 100;
+
+            /// <summary>
+            /// Intended to perform any CPU-bound work to free up the other threads to listen for packets
+            /// </summary>
+            private BackgroundWorker packetProcessorWorker;
+
+            public Interface(LivePacketDevice device, Queue<Host> outputQueue)
+            {
+                this.Device = device;
+                this.outputQueue = outputQueue;
+            }
+
+            public bool Listening
+            {
+                get { return Communicator != null; }
+                set
+                {
+                    if(value)
+                    {
+                        if(Listening)
+                        {
+                            StopListening();
+                        }
+                        StartListening();
+                    } else
+                    {
+                        StopListening();
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Cease listening on this interface
+            /// </summary>
+            private void StopListening()
+            {
+                // TODO
+            }
+
+            /// <summary>
+            /// Launch a background thread to listen for packets on this interface
+            /// </summary>
+            private void StartListening()
+            {
+                foreach (DeviceAddress addr in Device.Addresses)
+                {
+                    if (addr.Address.Family == SocketAddressFamily.Internet)
+                    {
+                        IpV4Address = addr;
+                    }
+                }
+
+                try
+                {
+                    Communicator = Device.Open(PACKET_RX_LEN_B, PacketDeviceOpenAttributes.Promiscuous, PACKET_RX_TIMEOUT_MS);
+                    // Filter to only IPv4 packets so we can assume they have an IPv4 header later
+                    using (BerkeleyPacketFilter filter = Communicator.CreateFilter("ip"))
+                    {
+                        Communicator.SetFilter(filter);
+                    }
+
+
+                    if (Communicator.DataLink.Kind != DataLinkKind.Ethernet)
+                    {
+                        Console.WriteLine("This program works only on Ethernet networks; skipping interface named " + Device.Name + " (" + Device.Description + ")");
+                        Communicator = null;
+                    }
+                    else
+                    {
+                        MonitorWorker = new BackgroundWorker();
+                        MonitorWorker.DoWork += processPackets;
+                        MonitorWorker.WorkerSupportsCancellation = true;
+                        MonitorWorker.WorkerReportsProgress = false;
+                        MonitorWorker.RunWorkerAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to open interface named " + Device.Name + " (" + Device.Description + "): " + ex);
+                    Communicator = null;
+                }
+            }
+
+            /// <summary>
+            /// Loop through packets until cancelled
+            /// </summary>
+            /// <param name="sender">Assumed to be the parent BackgroundWorker object</param>
+            /// <param name="e">Assumed to be the NetworkMonitor.Interface object being listened to</param>
+            private void processPackets(object sender, DoWorkEventArgs e)
+            {
+                BackgroundWorker worker = sender as BackgroundWorker;
+                try
+                {
+                    while (!worker.CancellationPending)
+                    {
+                        Packet packet;
+                        // We're only listening during the below function call.  Thus, all of this thread outside of this function call should be as fast as it can be.
+                        PacketCommunicatorReceiveResult result = Communicator.ReceivePacket(out packet);
+                        //communicator.ReceiveSomePackets()
+                        switch (result)
+                        {
+                            case PacketCommunicatorReceiveResult.Timeout:
+                                // Timeout elapsed
+                                break;
+                            case PacketCommunicatorReceiveResult.Ok:
+                                // Chuck it in the queue to evaluate on another thread
+                                Host host = new Host(packet.Ethernet.Source, packet.Ethernet.IpV4.Source, IpV4Address);
+                                lock (outputQueue)
+                                {
+                                    outputQueue.Enqueue(host);
+                                }
+                                break;
+                            default:
+                                throw new InvalidOperationException("The result " + result + " should never be reached here");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Caught exception in listener thread: " + ex);
+                }
+                finally
+                {
+                    Communicator = null;
+                }
+            }
         }
 
         /// <summary>
@@ -80,22 +223,10 @@ namespace PassiveAggressor
         /// Keys are device names
         /// </summary>
         public Dictionary<string, Interface> Interfaces { get; private set; } = new Dictionary<string, Interface>();
-
-        /// <summary>
-        /// Receive the first this many bytes of each packet
-        /// </summary>
-        private const int PACKET_RX_LEN_B = 1024;
-
-        /// <summary>
-        /// Check this often for worker cancellation
-        /// </summary>
-        private const int PACKET_RX_TIMEOUT_MS = 100;
-
         /// <summary>
         /// Intended to perform any CPU-bound work to free up the other threads to listen for packets
         /// </summary>
         private BackgroundWorker packetProcessorWorker;
-
         /// <summary>
         /// Find interfaces and open them all for listening
         /// </summary>
@@ -114,44 +245,9 @@ namespace PassiveAggressor
 
             foreach (LivePacketDevice device in allDevices)
             {
-                Interface intf = new Interface();
-                foreach (DeviceAddress addr in device.Addresses)
-                {
-                    if (addr.Address.Family == SocketAddressFamily.Internet)
-                    {
-                        intf.IpV4Address = addr;
-                    }
-                }
-
-                try
-                {
-                    intf.Communicator = device.Open(PACKET_RX_LEN_B, PacketDeviceOpenAttributes.Promiscuous, PACKET_RX_TIMEOUT_MS);
-                    // Filter to only IPv4 packets so we can assume they have an IPv4 header later
-                    using (BerkeleyPacketFilter filter = intf.Communicator.CreateFilter("ip"))
-                    {
-                        intf.Communicator.SetFilter(filter);
-                    }
-
-
-                    if (intf.Communicator.DataLink.Kind != DataLinkKind.Ethernet)
-                    {
-                        Console.WriteLine("This program works only on Ethernet networks; skipping interface named " + device.Name + " (" + device.Description + ")");
-                    }
-                    else
-                    {
-                        intf.Device = device;
-                        intf.MonitorWorker = new BackgroundWorker();
-                        intf.MonitorWorker.DoWork += processPackets;
-                        intf.MonitorWorker.WorkerSupportsCancellation = true;
-                        intf.MonitorWorker.WorkerReportsProgress = false;
-                        intf.MonitorWorker.RunWorkerAsync(intf);
-                        Interfaces.Add(device.Name, intf);
-                    }
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine("Failed to open interface named " + device.Name + " (" + device.Description + "): " + ex);
-                }
+                Interface intf = new Interface(device, hostsToIncorporate);
+                Interfaces.Add(device.Name, intf);
+                intf.Listening = true;
             }
 
             packetProcessorWorker = new BackgroundWorker();
@@ -220,47 +316,6 @@ namespace PassiveAggressor
                     }
                     // An inelegant way to avoid spinlock - sleep for about 1/1000th of the update interval
                     System.Threading.Thread.Sleep((int)(UpdateIntervalSeconds) + 1);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Caught exception in listener thread: " + ex);
-            }
-        }
-
-        /// <summary>
-        /// Loop through packets until cancelled
-        /// </summary>
-        /// <param name="sender">Assumed to be the parent BackgroundWorker object</param>
-        /// <param name="e">Assumed to be the NetworkMonitor.Interface object being listened to</param>
-        private void processPackets(object sender, DoWorkEventArgs e)
-        {
-            BackgroundWorker worker = sender as BackgroundWorker;
-            Interface intf = e.Argument as Interface;
-            try
-            {
-                while (!worker.CancellationPending)
-                {
-                    Packet packet;
-                    // We're only listening during the below function call.  Thus, all of this thread outside of this function call should be as fast as it can be.
-                    PacketCommunicatorReceiveResult result = intf.Communicator.ReceivePacket(out packet);
-                    //communicator.ReceiveSomePackets()
-                    switch (result)
-                    {
-                        case PacketCommunicatorReceiveResult.Timeout:
-                            // Timeout elapsed
-                            break;
-                        case PacketCommunicatorReceiveResult.Ok:
-                            // Chuck it in the queue to evaluate on another thread
-                            Host host = new Host(packet.Ethernet.Source, packet.Ethernet.IpV4.Source, intf.IpV4Address);
-                            lock (hostsToIncorporate)
-                            {
-                                hostsToIncorporate.Enqueue(host);
-                            }
-                            break;
-                        default:
-                            throw new InvalidOperationException("The result " + result + " should never be reached here");
-                    }
                 }
             }
             catch (Exception ex)
